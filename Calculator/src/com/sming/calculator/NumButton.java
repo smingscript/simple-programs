6e80379a2ca5b182431a3b94d79638c9de028946
수민 김 <strobe410glitter@gmail.com>
2018-03-23 17:55:16 +0900

    trouble shooting the Operator class

--- Calculator/src/com/sming/calculator/NumButton.java
+++ Calculator/src/com/sming/calculator/NumButton.java
@@ -15,27 +15,37 @@ public class NumButton implements ActionListener{
     private LinkedList<String> equation;
     private StringBuilder returnNum = new StringBuilder();
     private StringBuilder tempNum = new StringBuilder();
+    private String total;
 
     public NumButton(JTextField valueField, String btnText, LinkedList<String> equation) {
         this.valueField= valueField;
-        valueField.setText("");
+        //valueField.setText("");
         this.btnText = btnText;
         this.equation  = equation;
     }
 
     @Override
     public void actionPerformed(ActionEvent arg0) { 
-    	String total = "";
+    	
+    	if(returnNum.length() == 0) {
+    		total = "";
+    	}
+    	
     	System.out.println("this is tempNum before : " + tempNum);
         tempNum.append(btnText) ;
         System.out.println("this is tempNum: " + tempNum);
-        returnNum.append(valueField.getText());
+        //returnNum.append(valueField.getText());
         returnNum.append(tempNum);
         System.out.println("this is returnNum: " + returnNum);
         for(int i = 0; i < returnNum.length(); i++) {
         	total += returnNum.substring(i);
         }
+        System.out.println("this is total: " + total);
+        tempNum.deleteCharAt(0);
+        System.out.println("this is tempNum: " + tempNum);
         valueField.setText(total);
+        System.out.println("Hi!");
         equation.add(total);
+        returnNum.deleteCharAt(0);
     }
 }
